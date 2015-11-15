95f54929e04912ac42cc7cd168112ce695c7c883
Anargyros Akrivos <argy@akrivos.me>
2015-11-15 16:58:40 +0000

    Refactored CheckoutService to include Orange offer & got the tests passing

--- src/main/scala/me/akrivos/CheckoutService.scala
+++ src/main/scala/me/akrivos/CheckoutService.scala
@@ -18,10 +18,19 @@ class DefaultCheckoutService(implicit ec: ExecutionContext) extends CheckoutServ
     val items = basket.items.groupBy(x => x).map(x => (x._1, x._2.size))
 
     // buy one, get one free on Apples
-    items.get(Apple).filter(_ >= 2).map { count =>
+    val appleSavings: BigDecimal = items.get(Apple).filter(_ >= 2).map { count =>
       val total = count * Apple.price
       val offer = ((count / 2) * Apple.price) + ((count % 2) * Apple.price)
       total - offer
     }.getOrElse(0)
+
+    // 3 for the price of 2 on Oranges
+    val orangeSavings: BigDecimal = items.get(Orange).filter(_ >= 3).map { count =>
+      val total = count * Orange.price
+      val offer = ((count / 3) * 2 * Orange.price) + ((count % 3) * Orange.price)
+      total - offer
+    }.getOrElse(0)
+
+    appleSavings + orangeSavings
   }
 }
