aac43e989877f2bf4990941e2d86ee66cd76168e
Anargyros Akrivos <argy@akrivos.me>
2015-11-15 18:27:12 +0000

    Added failing test for JSON (un)marshalling

--- src/test/scala/me/akrivos/CheckoutApiTests.scala
+++ src/test/scala/me/akrivos/CheckoutApiTests.scala
@@ -1,11 +1,12 @@
 package me.akrivos
 
+import org.json4s._
 import org.scalatest.{FlatSpecLike, Matchers}
 import spray.http.StatusCodes._
 import spray.routing.HttpService
 import spray.testkit.ScalatestRouteTest
 
-class CheckoutApiTests extends FlatSpecLike with Matchers with ScalatestRouteTest with HttpService {
+class CheckoutApiTests extends FlatSpecLike with Matchers with ScalatestRouteTest with HttpService with JsonSupport {
 
   val actorRefFactory = system
   val routes = new CheckoutApi(new DefaultCheckoutService()).routes
@@ -14,6 +15,29 @@ class CheckoutApiTests extends FlatSpecLike with Matchers with ScalatestRouteTes
 
   it should "return 400 if it is not given a basket" in {
     Post("/checkout") ~> sealRoute(routes) ~> check {
+      status shouldBe BadRequest
+    }
+  }
+
+  it should "return 200 and a receipt of £0.6 for an Apple" in {
+    val json = JObject("items" -> JArray(List(JString("Apple"))))
+    Post("/checkout", json) ~> routes ~> check {
+      status shouldBe OK
+      responseAs[Receipt] shouldBe Receipt(List(Apple), 0.6)
+    }
+  }
+
+  it should "return 200 and a receipt of £0.25 for an Orange" in {
+    val json = JObject("items" -> JArray(List(JString("Orange"))))
+    Post("/checkout", json) ~> routes ~> check {
+      status shouldBe OK
+      responseAs[Receipt] shouldBe Receipt(List(Orange), 0.25)
+    }
+  }
+
+  it should "return 400 if an item in the basket is unrecognised" in {
+    val json = JObject("items" -> JArray(List(JString("XYZ"))))
+    Post("/checkout", json) ~> sealRoute(routes) ~> check {
       status shouldBe BadRequest
     }
   }
