7f272481e843b770f561b8960e217aa352c866ea
Anargyros Akrivos <argy@akrivos.me>
2015-11-15 18:32:14 +0000

    Added JSON support for Orange & Apple

--- src/main/scala/me/akrivos/JsonSupport.scala
+++ src/main/scala/me/akrivos/JsonSupport.scala
@@ -1,9 +1,15 @@
 package me.akrivos
 
-import org.json4s.DefaultFormats
+import org.json4s.JsonAST.JString
+import org.json4s.{CustomSerializer, DefaultFormats}
 import spray.httpx.Json4sJacksonSupport
 
 trait JsonSupport extends Json4sJacksonSupport {
 
-  override implicit val json4sJacksonFormats = DefaultFormats
+  object ItemSerialiser extends CustomSerializer[Item](format => (
+    { case JString(s) if Item.exists(s) => Item(s) },
+    { case x: Item => JString(x.name) }
+  ))
+
+  override implicit val json4sJacksonFormats = DefaultFormats + ItemSerialiser
 }
--- src/main/scala/me/akrivos/Models.scala
+++ src/main/scala/me/akrivos/Models.scala
@@ -1,14 +1,23 @@
 package me.akrivos
 
+object Item {
+  private val all = Set(Apple, Orange).map(x => (x.name, x)).toMap
+  def exists(str: String): Boolean = all.contains(str)
+  def apply(str: String): Item = all(str)
+}
+
 sealed trait Item {
+  def name: String
   def price: BigDecimal
 }
 
 case object Apple extends Item {
-  def price: BigDecimal = 0.6
+  val name = "Apple"
+  val price: BigDecimal = 0.6
 }
 
 case object Orange extends Item {
+  val name = "Orange"
   val price: BigDecimal = 0.25
 }
 
